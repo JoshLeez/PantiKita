d394b7a72ebf7553872b54d6f38b43797b38e993
JoshLeez <109338498+JoshLeez@users.noreply.github.com>
2022-11-14 17:39:44 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Beranda from './Component/Beranda'
+import Hubungikami from './Component/Hubungikami'
+import Footer from './Component/Footer'
+import Navbar from './Component/Navbar'
+import Pantiasuhankita from './Component/Pantiasuhankita';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Beranda />}/>
+           <Route path="/hubungikami" element={<Hubungikami />}/>
+           <Route path="/pantiasuhankita" element={<Pantiasuhankita />}/>
+        </Routes>
+        <Footer/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
